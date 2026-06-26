72e948d01c94efaf39b2e5703333783311baa4f8
tapascada <tapascada@gmail.com>
2026-06-26 11:04:02 -0500

    Add files via upload

--- serve.py
+++ serve.py
@@ -8,7 +8,7 @@ mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('application/json', '.json')
 mimetypes.add_type('image/png', '.png')
 
-PORT = 8081
+PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
 class MyHTTPRequestHandler(Handler):
@@ -19,5 +19,5 @@ class MyHTTPRequestHandler(Handler):
 
 # Use ThreadingHTTPServer to handle multiple concurrent browser requests without blocking
 with http.server.ThreadingHTTPServer(("", PORT), MyHTTPRequestHandler) as httpd:
-    print(f"KIKES MUNDIAL ADMIN PWA - Serving at http://localhost:{PORT}")
+    print(f"Serving at port {PORT}")
     httpd.serve_forever()
